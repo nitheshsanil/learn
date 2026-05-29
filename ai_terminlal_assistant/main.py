2e24331685679061d80744de77bfcd5b21d49a49
Nithesh Sanil <nitheshblaze625@gmail.com>
2026-05-30 00:13:24 +0530

    improvements

--- ai_terminlal_assistant/main.py
+++ ai_terminlal_assistant/main.py
@@ -1,11 +1,17 @@
-from memory import initialize_memory,save_conversation
+from memory import initialize_memory,save_conversation,reset_memory
 from llm import generate_response
 
 print("loading conversation")
 conversation = initialize_memory()
-
+print(conversation)
 while True:
     user_input = str(input("\n You : "))
+    if user_input.lower() == "/help":
+        print('Available commands:\n/help\n/clear\n/save\n/exit')
+    if user_input.lower() == "/save":
+        save_conversation(conversation)
+    if user_input.lower() == "/clear":
+        conversation = reset_memory()
     if user_input.lower() == "exit":
         save_conversation(conversation)
         break
@@ -18,3 +24,9 @@ while True:
 print("saving conversation......")
 save_conversation(conversation)
 print("conversation saved")
+
+
+# /help
+# /save
+# /clear
+# cleaner /exit
--- ai_terminlal_assistant/memory.py
+++ ai_terminlal_assistant/memory.py
@@ -1,6 +1,10 @@
 import os 
 import json
 
+
+system_prompt = [{"role":"system",
+                "content": "You are a strict AI mentor helping a developer improve quickly."}] 
+
 def initialize_memory():
     if os.path.isfile("chat_history.json"):
         conversation = load_memory()
@@ -10,8 +14,7 @@ def initialize_memory():
 
 def create_memory():
     with open("chat_history.json", "w", encoding="utf-8") as file:
-        conversation = [{"role":"system",
-                        "content": "You are a strict AI mentor helping a developer improve quickly."}] 
+        conversation = system_prompt
         json.dump(conversation,file,indent = 4)
     return conversation
 
@@ -19,10 +22,17 @@ def load_memory():
     print("chat history.json found || Continuing from memory.......")
     with open('chat_history.json','r', encoding = 'utf8') as file:
         conversation = json.load(file)
+        recent_prompts=conversation[-10:]
+        conversation=[*system_prompt,*recent_prompts] 
     return conversation
 
 def save_conversation(conversation):
     with open("chat_history.json","w",encoding='utf8')as f:
         json.dump(conversation,f,indent = 4)
 
+def reset_memory():
+    with open("chat_history.json", "w", encoding="utf-8") as file:
+        conversation = system_prompt
+        json.dump(conversation,file,indent = 4)
+    return conversation
 
